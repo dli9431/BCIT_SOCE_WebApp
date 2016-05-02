2f8dd8fad128fead3b3f25f485c98040390315ad
don <dli9431@gmail.com>
2016-05-02 10:46:56 -0700

    oops, here is the actual initial web app

---
+++ COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP4900_SOCE_WebApp.MvcApplication" Language="C#" %>
---
+++ COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP4900_SOCE_WebApp.Startup))]
+namespace COMP4900_SOCE_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
