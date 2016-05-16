5282b6a5eac9dd1d765e758b5f8284271c1b0367
don <dli9431@gmail.com>
2016-05-16 10:09:35 -0700

    changed csvreader to use lumenworks csvreader (sejong should already have something similar to this

--- COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Controllers/HomeController.cs
+++ COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
-﻿using MySql.Data.MySqlClient;
+﻿using LumenWorks.Framework.IO.Csv;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
+using System.Collections.Generic;
+
 
 namespace COMP4900_SOCE_WebApp.Controllers
 {
@@ -35,230 +41,172 @@ namespace COMP4900_SOCE_WebApp.Controllers
             return View();
         }
 
-        /*
-        public void sqlTest()
-        {
-            string connectionString = "server=127.0.0.1;database=soceweb;UserId=root;password=thisismysql;";
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                using (MySqlCommand cmd = connection.CreateCommand())
-                {
-                    cmd.CommandText = @"DROP TABLE test IF EXISTS";
-                    cmd.CommandText = @"CREATE TABLE test ( 
-                                            Id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                                            W2PlwMcD1MiSu DECIMAL(5,2),
-                                            W2PlwMcD2MiSu DECIMAL(5,2),
-                                            W2PlwMcD3MiSu DECIMAL(5,2),
-                                            W2PlwMcM1R1Su DECIMAL(5,2)
-                                        )";
-                    cmd.ExecuteNonQuery();
-                }
-                connection.Close();
-                connection.Open();
-
-                using (MySqlCommand insertCommand = connection.CreateCommand())
-                {
-                    insertCommand.CommandText =
-                        @"INSERT INTO test (W2PlwMcD1MiSu, W2PlwMcD2MiSu, W2PlwMcD3MiSu, W2PlwMcM1R1Su)
-                         
-                       VALUES (@W2PlwMcD1MiSu, @W2PlwMcD2MiSu, @W2PlwMcD3MiSu, @W2PlwMcM1R1Su)";
-
-                    insertCommand.Parameters.Add(new MySqlParameter("@W2PlwMcD1MiSu", "DECIMAL"));
-                    insertCommand.Parameters.Add(new MySqlParameter("@W2PlwMcD2MiSu", "DECIMAL"));
-                    insertCommand.Parameters.Add(new MySqlParameter("@W2PlwMcD3MiSu", "DECIMAL"));
-                    insertCommand.Parameters.Add(new MySqlParameter("@W2PlwMcM1R1Su", "DECIMAL"));
-
-                    string[] files = Directory.GetFiles("D:/computer/csv files", "*.csv");
-
-                    foreach (string file in files)
-                    {
-                        string[] lines = System.IO.File.ReadAllLines(file);
-                        bool parse = false;
-
-                        foreach (string tmpLine in lines)
-                        {
-                            string line = tmpLine.Trim();
-                            if (!parse && line.StartsWith("W2PlwMcD1MiSu"))
-                            {
-                                parse = true;
-                                continue;
-                            }
-                            if (!parse || string.IsNullOrEmpty(line))
-                            {
-                                continue;
-                            }
-
-                            foreach (MySqlParameter parameter in insertCommand.Parameters)
-                            {
-                                parameter.Value = null;
-                            }
-
-                            string[] values = line.Split(new[] { ',' });
-
-                            for (int i = 0; i < values.Length; i++)
-                            {
-                                MySqlParameter param = insertCommand.Parameters[i];
-
-                                decimal value;
-                                param.Value = decimal.TryParse(values[i], out value) ? value : 0;
-                                                         
-                            }
-                            insertCommand.ExecuteNonQuery();
-                        }
-                        
-                    }
-                }
-                connection.Close();
-            }            
-        }   
-        */
-
-
-        //public void sqlTest()
-        //{
-        //    string connectionString = "server=127.0.0.1;database=soceweb;UserId=root;password=thisismysql;";
-        //    using (MySqlConnection connection = new MySqlConnection(connectionString))
-        //    {
-        //        connection.Open();
-
-        //        string query = @"
-        //                CREATE TABLE test (                        
-        //                test1 varchar(255),
-        //                test2 varchar(255),
-        //                test3 varchar(255),
-        //                test4 varchar(255) 
-        //                )";
-
-        //        using (MySqlCommand cmd = new MySqlCommand(query, connection))
-        //        {
-        //            cmd.ExecuteNonQuery();
-        //            string[] files = Directory.GetFiles("D:/computer/csv files", "*.csv");
-
-        //            foreach (string file in files)
-        //            {
-        //                string tableName = "test";
-        //                query = "LOAD DATA INFILE 'D:/computer/csv files/test.csv' INTO TABLE " + tableName + " FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 LINES";
-
-        //                using (MySqlCommand cmd1 = new MySqlCommand(query, connection))
-        //                {
-        //                    cmd1.ExecuteNonQuery();
-        //                }
-
-        //            }
-        //        }
-
-        //    }
-        //}
         MySqlConnection connection;
-        string[] files = Directory.GetFiles("c:/sensor_data", "*.csv");
         string[] sensorNames;
-        string[] eachRow;
+        string[] sensorValues;
+        string[] dateTimeId;
 
         public void MySQLConnection()
         {
-            string connectionString = "server=127.0.0.1;database=soceweb;UserId=root;password=thisismysql;";
-            connection = new MySqlConnection(connectionString);
-            connection.Open();
+            //string connectionString = "server=127.0.0.1;database=soceweb;UserId=root;password=thisismysql;";
+            //connection = new MySqlConnection(connectionString);
+            //connection.Open();
         }
 
         public void sqlTest()
         {
-            MySQLConnection();
+            //MySQLConnection();
+            //string[] files = Directory.GetFiles("D:/computer/csv file", "*.csv");
+            string[] files = Directory.GetFiles("C:/sensor_data", "*.csv");
             string projectName;
-
-            //"hpws", "hpws_rp", "mh_north", "mh_south", 
-            //"th_int", "th_ext", "th_ps", "gvs_south", "gvs_north"
 
             for (int i = 0; i < files.Length; i++)
             {
-                Match match_filename_hpws = Regex.Match(files[i], @"^D:(.*)hpws_file(.*).csv$");
-                Match match_filename_hpws_rp = Regex.Match(files[i], @"^D:(.*)hpws_rp(.*).csv$");
-                Match match_filename_th_ext = Regex.Match(files[i], @"^D:(.*)th_ext(.*).csv$");
+                //Match match_filename_hpws = Regex.Match(files[i], @"^D:(.*)hpws_file(.*).csv$");
+                //Match match_filename_hpws_rp = Regex.Match(files[i], @"^D:(.*)hpws_rp(.*).csv$");
+                //Match match_filename_mh_north = Regex.Match(files[i], @"^D:(.*)mh_north(.*).csv$");
+                //Match match_filename_mh_south = Regex.Match(files[i], @"^D:(.*)mh_south(.*).csv$");
+                //Match match_filename_th_int = Regex.Match(files[i], @"^D:(.*)th_int(.*).csv$");
+                //Match match_filename_th_ext = Regex.Match(files[i], @"^D:(.*)th_ext(.*).csv$");
+                //Match match_filename_th_ps = Regex.Match(files[i], @"^D:(.*)th_ps(.*).csv$");
+                Match match_filename_gvs_south = Regex.Match(files[i], @"^C:\/sensor_data\/(.*)gvs_south(.*).csv$");
+                //Match match_filename_gvs_north = Regex.Match(files[i], @"^D:(.*)gvs_north(.*).csv$");
 
-                if (match_filename_hpws.Success)
-                {
-                    projectName = "hpws";
-                    process(match_filename_hpws.Value, projectName);
-                }
+                //if (match_filename_hpws.success)
+                //{
+                //    projectname = "hpws";
+                //    process(match_filename_hpws.value, projectname);
+                //}
                 //else if (match_filename_hpws_rp.Success)
                 //{
                 //    projectName = "hpws_rp";
                 //    process(match_filename_hpws_rp.Value, projectName);
+                //}
+                //else if (match_filename_mh_north.Success)
+                //{
+                //    projectName = "mh_north";
+                //    process(match_filename_mh_north.Value, projectName);
+                //}
+                //else if (match_filename_mh_south.Success)
+                //{
+                //    projectName = "mh_south";
+                //    process(match_filename_mh_south.Value, projectName);
+                //}
+                //else if (match_filename_th_int.Success)
+                //{
+                //    projectName = "th_int";
+                //    process(match_filename_th_int.Value, projectName);
                 //}
                 //else if (match_filename_th_ext.Success)
                 //{
                 //    projectName = "th_ext";
                 //    process(match_filename_th_ext.Value, projectName);
                 //}
-            }
+                //else if (match_filename_th_ps.Success)
+                //{
+                //    projectName = "th_ps";
+                //    process(match_filename_th_ps.Value, projectName);
+                //}
+                if (match_filename_gvs_south.Success)
+                {
+                    projectName = "gvs_south";
+                    process(match_filename_gvs_south.Value, projectName);
+                }
+                //else if (match_filename_gvs_north.Success)
+                //{
+                //    projectName = "gvs_north";
+                //    process(match_filename_gvs_north.Value, projectName);
+                //}
+            }    
         }
 
         public void process(string csvFile, string projectName)
         {
+            string connectionString = "server=127.0.0.1;database=soceweb;UserId=root;password=thisismysql;";
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+
             using (MySqlCommand insertCommand = connection.CreateCommand())
             {
-                insertCommand.CommandText =
-                    @"INSERT INTO " + projectName + "(SensorName) VALUES (@SensorName)";
-                //insertCommand.Parameters.AddWithValue("@Part_Number", partNumber);
-                insertCommand.Parameters.Add(new MySqlParameter("@SensorName", "String"));
+                insertCommand.CommandText = @"INSERT INTO " + projectName + "(SensorName, SensorValue, DateTime) VALUES (@SensorName, @SensorValue, @DateTime)";
+                insertCommand.Parameters.Add(new MySqlParameter("@SensorName", MySqlDbType.String));
+                insertCommand.Parameters.Add(new MySqlParameter("@SensorValue", MySqlDbType.Decimal));
+                insertCommand.Parameters.Add(new MySqlParameter("@DateTime", MySqlDbType.DateTime));
+            
 
-                string[] lines = System.IO.File.ReadAllLines(csvFile);
+                //List<string> sensors = new List<string>();
+                using (var csv = new CsvReader(new StreamReader(csvFile), true))
+            {
+                int fieldCount = csv.FieldCount;
+                string[] headers = csv.GetFieldHeaders().Skip(1).ToArray();
 
-
-                foreach (string line in lines)
-                {
-                    string trimedLine = line.Trim();
-
-                    if (line.StartsWith("Sensor"))
+                    while (csv.ReadNextRecord())
                     {
-                        sensorNames = trimedLine.Split(new[] { ',' }).Skip(1).ToArray();
+                        for (int i = 1; i < fieldCount-1; i++)
+                        {
+                            DateTime parsedDateTime = DateTime.ParseExact(csv[0], "dd/MMM/yyyy H:mm:ss", CultureInfo.InvariantCulture);
 
-                        continue;
-                    }
+                            MySqlParameter sensorName = insertCommand.Parameters[0];
+                            sensorName.Value = headers[i];
 
-                    eachRow = trimedLine.Split(new[] { ',' }).Skip(1).ToArray();
+                            MySqlParameter sensorValue = insertCommand.Parameters[1];
+                            sensorValue.Value = csv[i];
 
-                    //foreach (MySqlParameter parameter in insertCommand.Parameters)
-                    //{
-                    //    parameter.Value = null;
-                    //}
+                            MySqlParameter dateTime = insertCommand.Parameters[2];
+                            dateTime.Value = parsedDateTime;
 
-                    for (int i = 0; i < sensorNames.Length; i++)
-                    {
-                        MySqlParameter param = insertCommand.Parameters[0];
-                        param.Value = sensorNames[i];
-                        insertCommand.ExecuteNonQuery();
+                            insertCommand.ExecuteNonQuery();
+
+                        }
                     }
-                    /*
-                    for (int i = 0; i < eachRow.Length; i++)
-                    {
-                        MySqlParameter param = insertCommand.Parameters[1];
-                        param.Value = eachRow[i];
-                       // insertCommand.ExecuteNonQuery();
-                    }
-                    int[] datetime = { 1, 2, 3, 4, 5, 6, 7, 8 };
-                    for (int i = 0; i < datetime.Length; i++)
-                    {
-                        MySqlParameter param = insertCommand.Parameters[2];
-                        param.Value = datetime[i];
-                      //  insertCommand.ExecuteNonQuery();
-                    }
-                    //  insertCommand.ExecuteNonQuery();
-                    */
                 }
-                insertCommand.CommandText =
-                   @"INSERT INTO " + projectName + "(SensorValue) VALUES (@SensorValue)";
-                insertCommand.Parameters.Add(new MySqlParameter("@SensorValue", "DECIMAL"));
-
-                insertCommand.CommandText =
-                   @"INSERT INTO " + projectName + "(DateTimeId) VALUES (@DateTimeId)";
-                insertCommand.Parameters.Add(new MySqlParameter("@DateTimeId", "DECIMAL"));
-
-
-
             }
-        }
+
+            connection.Close();
+
+
+
+            //using (MySqlCommand insertCommand = connection.CreateCommand())
+            //{
+            //    insertCommand.CommandText = @"INSERT INTO " + projectName + "(SensorName, SensorValue, DateTime) VALUES (@SensorName, @SensorValue, @DateTime)";
+            //    insertCommand.Parameters.Add(new MySqlParameter("@SensorName", MySqlDbType.String));
+            //    insertCommand.Parameters.Add(new MySqlParameter("@SensorValue", MySqlDbType.Decimal));
+            //    insertCommand.Parameters.Add(new MySqlParameter("@DateTime", MySqlDbType.DateTime));
+
+            //    //string[] lines = System.IO.File.ReadAllLines(csvFile);
+
+            //    foreach (string line in System.IO.File.ReadLines(csvFile))
+            //    {
+            //        string trimedLine = line.Trim();
+
+            //        if (line.StartsWith("Sensor"))
+            //        {
+            //            sensorNames = trimedLine.Split(',').Skip(1).ToArray();
+            //            continue;
+            //        }
+
+            //        sensorValues = trimedLine.Split(new[] { ',' }).Skip(1).ToArray();
+            //        dateTimeId = trimedLine.Split(',').Take(1).ToArray();
+            //        string convertToString = string.Join("", dateTimeId);
+            //        DateTime parsedDateTime = DateTime.ParseExact(convertToString, "dd/MMM/yyyy H:mm:ss", CultureInfo.InvariantCulture);
+            //        //"dd/MMM/yyyy H:mm:ss"
+            //        // "MM/dd/yy H:mm"
+
+            //        for (int i = 0; i < sensorNames.Length; i++)
+            //        {
+            //            MySqlParameter sensorName = insertCommand.Parameters[0];
+            //            sensorName.Value = sensorNames[i];
+
+            //            MySqlParameter sensorValue = insertCommand.Parameters[1];
+            //            sensorValue.Value = sensorValues[i];
+
+            //            MySqlParameter dateTime = insertCommand.Parameters[2];
+            //            dateTime.Value = parsedDateTime;
+
+            //            insertCommand.ExecuteNonQuery();
+            //        }
+            //    }
+            //}
+        }        
     }
 }
-
---
+++ COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Controllers/HomeController_old.cs
@@ -0,0 +1,170 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace COMP4900_SOCE_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult csv()
+        {
+            sqlTest();
+            return View();
+        }
+
+    
+        MySqlConnection connection;
+        string[] files = Directory.GetFiles("c:/sensor_data", "*.csv");
+        string[] sensorNames;
+        string[] eachRow;
+
+        public void MySQLConnection()
+        {
+            string connectionString = "server=127.0.0.1;database=soceweb;UserId=root;password=thisismysql;";
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void sqlTest()
+        {
+            MySQLConnection();
+            string projectName;
+
+            //"hpws", "hpws_rp", "mh_north", "mh_south", 
+            //"th_int", "th_ext", "th_ps", "gvs_south", "gvs_north"
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                projectName = "hpws";
+                string path = "C:\\sensor_data\\hpws.csv";
+                process(path, projectName);
+
+                //Match match_filename_hpws = Regex.Match(files[i], @"^C:\\sensor_data\\hpws.csv$");
+                //Match match_filename_hpws_rp = Regex.Match(files[i], @"^D:(.*)hpws_rp(.*).csv$");
+                //Match match_filename_th_ext = Regex.Match(files[i], @"^D:(.*)th_ext(.*).csv$");
+
+                //if (match_filename_hpws.Success)
+                //{
+                //    projectName = "hpws";
+                //    process(match_filename_hpws.Value, projectName);
+                //}
+                //else if (match_filename_hpws_rp.Success)
+                //{
+                //    projectName = "hpws_rp";
+                //    process(match_filename_hpws_rp.Value, projectName);
+                //}
+                //else if (match_filename_th_ext.Success)
+                //{
+                //    projectName = "th_ext";
+                //    process(match_filename_th_ext.Value, projectName);
+                //}
+            }
+        }
+
+        public void process(string csvFile, string projectName)
+        {
+            using (MySqlCommand insertCommand = connection.CreateCommand())
+            {
+                insertCommand.CommandText =
+                    @"INSERT INTO " + projectName + "(SensorName) VALUES (@SensorName)";
+                //insertCommand.Parameters.AddWithValue("@Part_Number", partNumber);
+                insertCommand.Parameters.Add(new MySqlParameter("@SensorName", "String"));
+
+                using (CsvReader csv =
+           new CsvReader(new StreamReader(csvFile), true))
+                {
+                    while (csv.ReadNextRecord())
+                    {
+                        MySqlParameter param = insertCommand.Parameters[0];
+                        param.value = ""
+                        insertCommand.Parameters.Add(new MySqlParameter("@SensorValue", "DECIMAL"));
+                        //param.Value = "dddd";
+                        insertCommand.ExecuteNonQuery();
+                    }
+                }
+
+
+
+                //string[] lines = System.IO.File.ReadAllLines(csvFile);
+
+
+                //foreach (string line in lines)
+                //{
+                //    string trimedLine = line.Trim();
+
+                //    if (line.StartsWith("Sensor"))
+                //    {
+                //        sensorNames = trimedLine.Split(new[] { ',' }).Skip(1).ToArray();
+
+                //        continue;
+                //    }
+
+                //    eachRow = trimedLine.Split(new[] { ',' }).Skip(1).ToArray();
+
+                //    foreach (MySqlParameter parameter in insertCommand.Parameters)
+                //    {
+                //        parameter.Value = null;
+                //    }
+
+                //    for (int i = 0; i < sensorNames.Length; i++)
+                //    {
+                //        MySqlParameter param = insertCommand.Parameters[0];
+                //        param.Value = sensorNames[i];
+                //        insertCommand.ExecuteNonQuery();
+                //    }
+
+                //    for (int i = 0; i < eachRow.Length; i++)
+                //    {
+                //        MySqlParameter param = insertCommand.Parameters[1];
+                //        param.Value = eachRow[i];
+                //        insertCommand.ExecuteNonQuery();
+                //    }
+                //    int[] datetime = { 1, 2, 3, 4, 5, 6, 7, 8 };
+                //    for (int i = 0; i < datetime.Length; i++)
+                //    {
+                //        MySqlParameter param = insertCommand.Parameters[2];
+                //        param.Value = datetime[i];
+                //        insertCommand.ExecuteNonQuery();
+                //    }
+                //    insertCommand.ExecuteNonQuery();
+
+                //}
+                //insertCommand.CommandText =
+                //   @"INSERT INTO " + projectName + "(SensorValue) VALUES (@SensorValue)";
+                //insertCommand.Parameters.Add(new MySqlParameter("@SensorValue", "DECIMAL"));
+
+                //insertCommand.CommandText =
+                //   @"INSERT INTO " + projectName + "(DateTimeId) VALUES (@DateTimeId)";
+                //insertCommand.Parameters.Add(new MySqlParameter("@DateTimeId", "DECIMAL"));
+
+
+
+            }
+        }
+    }
+}
+
