27a162c6b89a3cfb83ac49f39fef28a2847caa6c
kkagill <kkagill@gmail.com>
2016-05-20 12:23:09 -0700

    export controller

---
+++ COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Controllers/ExportController.cs
@@ -0,0 +1,22 @@
+﻿using COMP4900_SOCE_WebApp.Models;
+using SensorDataModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP4900_SOCE_WebApp.Controllers
+{
+    public class ExportController : Controller
+    {
+        private SensorContext db = new SensorContext();
+        private ApplicationDbContext db2 = new ApplicationDbContext();
+
+        // GET: Export
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
