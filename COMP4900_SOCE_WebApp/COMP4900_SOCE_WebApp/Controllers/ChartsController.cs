69e0a44dcb87c2cd340cdfb758e179cdf04d9860
kbyunghak <kbyunghak@gmail.com>
2016-05-24 02:32:21 -0700

    pass Sensorlist to controller

--- COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Controllers/ChartsController.cs
+++ COMP4900_SOCE_WebApp/COMP4900_SOCE_WebApp/Controllers/ChartsController.cs
@@ -15,10 +15,6 @@ namespace COMP4900_SOCE_WebApp.Controllers
     {
         private SensorContext db = new SensorContext();
 
-        string sensor1, sensor2, sensor3, sensor4, sensor5, sensor6;
-        List<String> sensorsPicked;
-        DateTime begin, end;
-
         public ActionResult DateTime()
         {
             return View();
@@ -47,19 +43,53 @@ namespace COMP4900_SOCE_WebApp.Controllers
             ViewData["sensor6"] = sensorNamesList;
             return View();
         }
+        string sensor1, sensor2, sensor3, sensor4, sensor5, sensor6;
 
         // GET: Charts
-        public ActionResult Report(DateTime FDTS, DateTime EDTS)
+        public ActionResult Report(string SensorList, DateTime? FDTS, DateTime? EDTS)
         {
+            string[] s = SensorList.Split(',');
 
-            return View();
+            
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                int x = i + 1;
+
+                string sensorName = "sensor" + x;
+                switch (sensorName)
+                {
+                    case "sensor1":
+                        sensor1 = Convert.ToString(s[i]);
+                        break;
+                    case "sensor2":
+                        sensor2 = Convert.ToString(s[i]);
+                        break;
+                    case "sensor3":
+                        sensor3 = Convert.ToString(s[i]);
+                        break;
+                    case "sensor4":
+                        sensor4 = Convert.ToString(s[i]);
+                        break;
+                    case "sensor5":
+                        sensor5 = Convert.ToString(s[i]);
+                        break;
+                    case "sensor6":
+                        sensor6 = Convert.ToString(s[i]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return GetCharts(sensor1, sensor2, sensor3, sensor4, sensor5, sensor6, FDTS, EDTS);
         }
 
-        public ActionResult GetCharts(string sensor1, string sensor2, string sensor3, string sensor4, string sensor5, string sensor6, DateTime FDTS, DateTime EDTS)
+        public ActionResult GetCharts(string sensor1, string sensor2, string sensor3, string sensor4, string sensor5, string sensor6, DateTime? FDTS, DateTime? EDTS)
         {
             //  Debug.Write("begin date: ");
-            FDTS = new DateTime(2016, 4, 22, 19, 27, 15);
-            EDTS = new DateTime(2016, 4, 30, 19, 27, 15);
+            //FDTS = new DateTime(2016, 4, 22, 19, 27, 15);
+            //EDTS = new DateTime(2016, 4, 30, 19, 27, 15);
             // var FfDTS = DateTime.ParseExact("04/22/2016 12:10", "MM/dd/yy H:mm", CultureInfo.InvariantCulture);
 
             var chkData = (from c in db.Sensors
